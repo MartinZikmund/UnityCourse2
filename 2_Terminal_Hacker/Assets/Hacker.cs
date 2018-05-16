6b9a8987de899424b9be2646749e790be80b6461
Martin Zikmund <martinzikmund@sphereline.com>
2018-05-16 13:02:00 +0200

    Terminal hacker

---
+++ 2_Terminal_Hacker/Assets/Hacker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Terminal.WriteLine("Choose difficulty");
+        Terminal.WriteLine("1 Easy");
+        Terminal.WriteLine("2 Medium");
+        Terminal.WriteLine("3 Hard");
+        Terminal.WriteLine("Enter your selection: ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
